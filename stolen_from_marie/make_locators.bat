c863752d5c0d7db8da5d9f104185b539c151a4b9
zai-tm <tusharmistry444@gmail.com>
2021-06-14 16:41:00 -0600

    locators

--- stolen_from_marie/make_locators.bat
+++
@@ -1,5 +0,0 @@
-set host=https://zai-tm.github.io/skinpacks/waypoint/loc/
-del *.jpg.loc
-del *.webm.loc
-for /r %%a in (*.png) do @echo %host%%%~nxa > %%~nxa.loc
-for /r %%a in (*.webm) do @echo %host%%%~nxa > %%~nxa.loc

f004c929958fc82e71ad9660b0ff5234995fcb0b
zai-tm <tusharmistry444@gmail.com>
2021-06-29 17:43:31 -0600

    fixes

--- rndbg2020.js
+++ rndbg2020.js
@@ -1,4 +1,4 @@
-var backgroundsCount = 4;
+var backgroundsCount = 6;
 var imagesNumber = Math.floor(Math.random() * backgroundsCount);
 console.log(imagesNumber)
-document.getElementById('main').style.backgroundImage = 'url(bgimages/tm2020/image' + imagesNumber + '.jpg)';
+document.getElementById('main').style.backgroundImage = 'url(https://zai-tm.github.io/bgimages/tm2020/image' + imagesNumber + '.jpg)';
